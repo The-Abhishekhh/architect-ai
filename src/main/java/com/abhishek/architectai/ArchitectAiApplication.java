77b14504ae460a2b62543785fdae4ca7abff061c
theabhishekh <abhikumar1324@gmail.com>
2026-06-23 21:17:39 +0530

    Initial Spring Boot setup for ArchitectAI

---
+++ src/main/java/com/abhishek/architectai/ArchitectAiApplication.java
@@ -0,0 +1,12 @@
+package com.abhishek.architectai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectAiApplication.class, args);
+	}
+}
