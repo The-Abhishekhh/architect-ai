31fc2e9b8128a48f6cadeb14f16a7883a3e68b85
theabhishekh <abhikumar1324@gmail.com>
2026-07-04 22:29:16 +0530

    Added structured logging to AI client

--- src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
+++ src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
@@ -1,24 +1,30 @@
 package com.abhishek.architectai.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AiEvaluationClient {
 
+    // Logger object
+    private static final Logger logger =
+            LoggerFactory.getLogger(AiEvaluationClient.class);
+
+    // Dependency Injection
     private final RestTemplate restTemplate;
 
-    public AiEvaluationClient(
-            RestTemplate restTemplate) {
-
+    public AiEvaluationClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public AiApiResponse evaluateAnswer(
-            AiApiRequest request) {
+    public AiApiResponse evaluateAnswer(AiApiRequest request) {
 
-        String url =
-                "https://jsonplaceholder.typicode.com/posts/1";
+        logger.info("Sending AI request for question: {}",
+                request.getQuestion());
+
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
 
         ExternalApiResponse response =
                 restTemplate.getForObject(
@@ -26,9 +32,10 @@ public class AiEvaluationClient {
                         ExternalApiResponse.class
                 );
 
+        logger.info("Received response from external API.");
+
         String feedback =
-                "External API says: " +
-                        response.getTitle();
+                "External API says: " + response.getTitle();
 
         return new AiApiResponse(feedback);
     }
