c5c80741f79f75d0a0769f9784a7ff2c410650b6
theabhishekh <abhikumar1324@gmail.com>
2026-07-05 20:19:18 +0530

    Refactored HTTP communication using HttpEntity and ResponseEntity

--- src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
+++ src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
@@ -2,17 +2,19 @@ package com.abhishek.architectai.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AiEvaluationClient {
 
-    // Logger object
     private static final Logger logger =
             LoggerFactory.getLogger(AiEvaluationClient.class);
 
-    // Dependency Injection
     private final RestTemplate restTemplate;
 
     public AiEvaluationClient(RestTemplate restTemplate) {
@@ -26,16 +28,27 @@ public class AiEvaluationClient {
 
         String url = "https://jsonplaceholder.typicode.com/posts/1";
 
-        ExternalApiResponse response =
-                restTemplate.getForObject(
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+
+        ResponseEntity<ExternalApiResponse> response =
+                restTemplate.exchange(
                         url,
+                        HttpMethod.GET,
+                        entity,
                         ExternalApiResponse.class
                 );
 
         logger.info("Received response from external API.");
 
         String feedback =
-                "External API says: " + response.getTitle();
+                "External API says: " +
+                        response.getBody().getTitle();
 
         return new AiApiResponse(feedback);
     }
