2958ac813a35eef7ddd7c6cf57c7214f7d6803d5
theabhishekh <abhikumar1324@gmail.com>
2026-07-08 19:31:13 +0530

    Migrated external API communication to WebClient

--- src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
+++ src/main/java/com/abhishek/architectai/client/AiEvaluationClient.java
@@ -2,12 +2,8 @@ package com.abhishek.architectai.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class AiEvaluationClient {
@@ -15,41 +11,29 @@ public class AiEvaluationClient {
     private static final Logger logger =
             LoggerFactory.getLogger(AiEvaluationClient.class);
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public AiEvaluationClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public AiEvaluationClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     public AiApiResponse evaluateAnswer(AiApiRequest request) {
 
-        logger.info("Sending AI request for question: {}",
-                request.getQuestion());
+        logger.info("Sending AI request...");
 
-        String url = "https://jsonplaceholder.typicode.com/posts/1";
+        ExternalApiResponse response =
+                webClient
+                        .get()
+                        .uri("https://jsonplaceholder.typicode.com/posts/1")
+                        .retrieve()
+                        .bodyToMono(ExternalApiResponse.class)
+                        .block();
 
+        logger.info("Response received.");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-
-        ResponseEntity<ExternalApiResponse> response =
-                restTemplate.exchange(
-                        url,
-                        HttpMethod.GET,
-                        entity,
-                        ExternalApiResponse.class
-                );
-
-        logger.info("Received response from external API.");
-
-        String feedback =
+        return new AiApiResponse(
                 "External API says: " +
-                        response.getBody().getTitle();
-
-        return new AiApiResponse(feedback);
+                        response.getTitle()
+        );
     }
 }
