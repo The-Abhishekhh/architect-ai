5274b6f559a6a62d1d31c64af77f80d91399f7d9
theabhishekh <abhikumar1324@gmail.com>
2026-07-09 19:17:31 +0530

    Added DTOs for Gemini request structure

---
+++ src/main/java/com/abhishek/architectai/dto/gemini/GeminiRequest.java
@@ -0,0 +1,16 @@
+package com.abhishek.architectai.dto.gemini;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeminiRequest {
+
+    private List<GeminiContent> contents;
+
+}
